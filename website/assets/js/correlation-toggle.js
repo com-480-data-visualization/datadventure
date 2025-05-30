532a608e678042fa1c9d4f50d38ec4ad203660ef
DavidHolzwarth <david.holzwarth@tum.de>
2025-05-30 11:09:59 +0200

    feat: make correlation format nicer

--- website/assets/js/correlation-toggle.js
+++ website/assets/js/correlation-toggle.js
@@ -2,14 +2,12 @@ const btnDepression = document.getElementById("btn-depression");
 const btnAnxiety = document.getElementById("btn-anxiety");
 const corr1 = document.getElementById("correlation1");
 const corr2 = document.getElementById("correlation2");
-const legendTitle = document.getElementById("legend-title");
 
 btnDepression.addEventListener("click", () => {
   corr1.style.display = "block";
   corr2.style.display = "none";
   btnDepression.classList.add("active");
   btnAnxiety.classList.remove("active");
-  legendTitle.textContent = "Depression";
 });
 
 btnAnxiety.addEventListener("click", () => {
@@ -17,5 +15,4 @@ btnAnxiety.addEventListener("click", () => {
   corr2.style.display = "block";
   btnAnxiety.classList.add("active");
   btnDepression.classList.remove("active");
-  legendTitle.textContent = "Anxiety";
 });
